ac224d7d69e9ddc94a9f6c27c4e0d64ca89c1427
Oleh Khalieiev <oleh.khalieiev@nure.ua>
2021-05-19 00:58:27 +0300

    Oleg Khalieiev
    Add Description

--- WebApplication3/Models/user.cs
+++ WebApplication3/Models/user.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -20,14 +21,19 @@ namespace WebApplication3.Models
 
         [Key]
         public int user_id { get; set; }
+        [DisplayName("Name")]
         public string username { get; set; }
         [Required]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        [DisplayName("Email")]
         public string email { get; set; }
         [Required]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
+        [DisplayName("Password")]
         public string password { get; set; }
+        [DisplayName("Role")]
         public string role { get; set; }
+        [DisplayName("Pnome number")]
         public string phone_number { get; set; }
        
 
