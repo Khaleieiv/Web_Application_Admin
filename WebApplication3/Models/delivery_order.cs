341879dbb9ed571e7a197e96ba0aa242930c60e5
Oleh Khalieiev <oleh.khalieiev@nure.ua>
2021-05-19 09:50:33 +0300

    Oleh Khalieiev
    Add edit vies

--- WebApplication3/Models/delivery_order.cs
+++ WebApplication3/Models/delivery_order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,20 +12,33 @@ namespace WebApplication3.Models
     public class delivery_order
     {
         [Key]
+        [DisplayName("Order Id")]
         public int order_id { get; set; }
+        [DisplayName("User Id")]
         public int user_id { get; set; }
+        [DisplayName("Data")]
         public DateTime? date { get; set; }
+        [DisplayName("Street")]
         public string street { get; set; }
+        [DisplayName("House ")]
         public string house { get; set; }
+        [DisplayName("Flat")]
         public string apartment_number { get; set; }
+        [DisplayName("Payment")]
         public bool need_payment { get; set; }
+        [DisplayName("Compled")]
         public bool completed { get; set; }
+        [DisplayName("Data complite")]
         public DateTime? completion_datetime { get; set; }
+        [DisplayName("Stars")]
         public int? stars { get; set; }
+        [DisplayName("Review")]
         public string review { get; set; }
+       
         public int? promocode_id { get; set; }
 
         public user us { get; set; }
+        [DisplayName("Promocode")]
         public promocode promo { get; set; }
 
     }
